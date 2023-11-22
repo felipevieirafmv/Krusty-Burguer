375b1fb4a80d1e2318da35149d3addf540419923
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2023-11-22 16:35:03 -0300

    Criacao da pagina de adm

---
+++ Front/src/app/adm-page/adm-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-adm-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './adm-page.component.html',
+  styleUrl: './adm-page.component.css'
+})
+export class AdmPageComponent {
+
+}
--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { AdmPageComponent } from './adm-page/adm-page.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginPageComponent}
+    { path: 'login', component: LoginPageComponent},
+    { path: 'adm', component: AdmPageComponent}
 ];
