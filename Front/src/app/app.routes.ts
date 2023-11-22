782b96c6725c3952f85da6e12f708cfe82134dbb
Felipe Vieira <127983939+felipevieirafmv@users.noreply.github.com>
2023-11-22 13:56:50 -0300

    Delete Front directory

--- Front/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginPageComponent } from './login-page/login-page.component';
-
-export const routes: Routes = [
-    { path: 'login', component: LoginPageComponent}
-];
