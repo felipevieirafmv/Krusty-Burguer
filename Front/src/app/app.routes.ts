b6468396cc87fbb18376c2e3f152470032d4fb03
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2023-11-22 16:09:13 -0300

    Finalizando a tela de login

---
+++ Front/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+export const routes: Routes = [
+    { path: 'login', component: LoginPageComponent}
+];
