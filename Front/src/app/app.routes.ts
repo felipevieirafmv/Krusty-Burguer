09870044063f377ae338a30c69f2c10cf01c3af8
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2023-11-23 16:49:34 -0300

    Adicionando imagens do cardapio

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AdmPageComponent } from './adm-page/adm-page.component';
 import { PedidosPageComponent } from './pedidos-page/pedidos-page.component';
+import { ProdutosPageComponent } from './produtos-page/produtos-page.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginPageComponent},
     { path: 'adm', component: AdmPageComponent},
     { path: 'pedidos', component: PedidosPageComponent},
+    { path: 'produtos', component: ProdutosPageComponent},
 ];
