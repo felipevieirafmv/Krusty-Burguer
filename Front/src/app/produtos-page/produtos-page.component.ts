06de61efbf63ae5b28cf064bce514d6d4f9d764c
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2023-11-23 16:01:01 -0300

    Criacao da tela produtos

---
+++ Front/src/app/produtos-page/produtos-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-produtos-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './produtos-page.component.html',
+  styleUrl: './produtos-page.component.css'
+})
+export class ProdutosPageComponent {
+
+}
