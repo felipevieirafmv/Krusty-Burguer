cdaba69fa7fb0490ed81c1e504996b9c85197f5d
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2023-11-24 14:14:55 -0300

    Criando pagina do totem

---
+++ Front/src/app/totem-page/totem-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-totem-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './totem-page.component.html',
+  styleUrl: './totem-page.component.css'
+})
+export class TotemPageComponent {
+
+}
