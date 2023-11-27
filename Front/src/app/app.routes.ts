545eb14c0bca37f5d4ad54d3b19ca24b8d51c95c
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2023-11-27 16:53:36 -0300

    Desenvolvimento do totem

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { AdmPageComponent } from './adm-page/adm-page.component';
 import { PedidosPageComponent } from './pedidos-page/pedidos-page.component';
 import { ProdutosPageComponent } from './produtos-page/produtos-page.component';
+import { TotemPageComponent } from './totem-page/totem-page.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginPageComponent},
     { path: 'adm', component: AdmPageComponent},
     { path: 'pedidos', component: PedidosPageComponent},
-    { path: 'produtos', component: ProdutosPageComponent},
+    { path: 'totem', component: TotemPageComponent},
 ];
